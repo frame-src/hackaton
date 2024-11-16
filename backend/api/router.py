866cd0c8a2ca52c16ab1f4f163402aca0183ecdf
trosinsk <120689775+trosinski08@users.noreply.github.com>
2024-11-16 11:38:09 +0000

    data

--- backend/api/router.py
+++ backend/api/router.py
@@ -1,5 +1,6 @@
 from model.model import CarbonDioxideConversionResponse
 from fastapi import APIRouter
+from core.co2_calc import calc_co2
 
 
 router = APIRouter()
@@ -7,6 +8,7 @@ router = APIRouter()
 # average CO₂ emissions per kilometer = 149 gCO2/km
 @router.get('/CarbonDioxideBalance')
 async def carbonDioxideBalance( km: int) :
-    conv = (km * 149)/1000
-    response = CarbonDioxideConversionResponse(unit="KgCO2/km", result=conv)
+
+	resp_val = calc_co2(row)
+    response = CarbonDioxideConversionResponse(unit="KgCO2/km", result=resp_val)
     return response
