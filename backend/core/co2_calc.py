bca9c22494a9d82f023e88a25173ec2eddf1c60c
Francesco Messina <iframe@live.it>
2024-11-16 12:37:18 +0100

    fix: load data works

--- backend/core/co2_calc.py
+++ backend/core/co2_calc.py
@@ -1,24 +1,19 @@
 import pandas as pd
-median_co2_g_km = 120 
 
-# data = '/content/data.csv'
-# df = pd.read_csv(data)
-
-# Apply the calc_co2 function to each row of the DataFrame
-
-def calc_co2(rows : any):
-  
-  for row in rows:
-    print(row)
-    if row['Mode'] in ['walk', 'bicycle']:
-      co2_kg = -1 * (rows['Length']) * (median_co2_g_km) / 1000
-    else:
-      co2_kg = (rows['Length']) * (median_co2_g_km) / 1000
-    
-  return round(co2_kg, 2)
+MEDIAN_CO2_G_KM = 120 
 
 
-# df['CO2_kg'] = df.apply(calc_co2, axis=1) 
 
+def calc_co2(rows: pd.DataFrame) -> pd.Series:
+    co2_kg_values = []
+    for _, row in rows.iterrows():        
+        if row['Mode'] in ['walk', 'bicycle']:
+            co2_kg = -1 * (row['Length']) * (MEDIAN_CO2_G_KM) / 1000
+        else:
+            co2_kg = (row['Length']) * (MEDIAN_CO2_G_KM) / 1000
+        
+        co2_kg_values.append(round(co2_kg, 2))
+
+    return co2_kg_values
 
 ### https://colab.research.google.com/drive/1fZDtLZBprzKM26kI7w6EjbaYyUPX6byD?usp=sharing
